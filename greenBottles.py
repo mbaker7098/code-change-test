a66d687061913988535a759641b0134a7a798b82
ASquishedPumpkinAndGoats <mbaker7098@sohamcollege.org.uk>
2014-05-08 12:10:00 +0100

    Sick update XD
    added a loop(for) and comments

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,7 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+bottles = 10
+for x in range(bottles,0,-1):#counts down a for loop until it reaches 0
+    print("{0} green bottles, hanging on the wall\n{0} green bottles, hanging on the wall".format(x))#prints bottles within the {0}
+    print("And if 1 green bottle should acidentally fall,\nThey'll be {0} green bottles hanging on the wall\n\n\n\n".format(x-1))
+    time.sleep(3)#prints every verse after 3 seconds
+        
